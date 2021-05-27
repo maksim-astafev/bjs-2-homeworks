5c9c95b24c6123c8dfc96fc527e2d503e90a6d91
Maksim Astafev <maksim.astafev@gmail.com>
2021-05-27 21:45:07 +0300

    ДЗ-3 выполнено в task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,12 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  return arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]);
 }
+// const compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]);
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 
   return resultArr; // array
 }
