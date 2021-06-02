f9b5baae4cfa440fd4538ac6ed56e464c36f3751
Maksim Astafev <maksim.astafev@gmail.com>
2021-06-02 15:32:16 +0300

    ДЗ-4 выполнено в task.js и исправлена ошибка excludeStudent -> exclude в main.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,55 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let students = [];
+students[0] = new Student("Петров Пётр Петрович", 1, 19);
+students[1] = new Student("Иванов Иван Иванович", 1, 18);
+students[2] = new Student("Михайлова Мария Михайловна", 0, 19);
+students[3] = new Student("Алексеева Анастасия Алексеевна", 0, 18);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function addMark(mark) {
+  if(this.marks === undefined){ 
+    this.marks = [];
+  }
+  
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function addMarks(...marksList) {
+  if(this.marks === undefined){ 
+    this.marks = [];
+  }
+  
+  marksList.forEach((mark) => this.marks.push(mark));
+}
+
+Student.prototype.getAverage = function getAverage() {
+  if(this.marks === undefined){ 
+    return "Нет оценок";
+  } else {
+    let sum = 0;
+    for(const mark of this.marks) {
+      sum += mark;
+    }
+
+    return sum/this.marks.length;
+  }
+}
+
+Student.prototype.exclude = function exclude(reason) {
+  if(this.marks !== undefined){ 
+    delete this.marks;
+  }
+  if(this.subject !== undefined){ 
+    delete this.subject;
+  }
+
+  this.excluded = reason;
+}
