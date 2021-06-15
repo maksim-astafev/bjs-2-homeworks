dd37d90627c446b54294bef33692400425864482
Maksim Astafev <maksim.astafev@gmail.com>
2021-06-15 18:51:41 +0300

    ДЗ-7 выполнено в task.js и проверка в main.js

--- 7.decorators/main.js
+++ 7.decorators/main.js
@@ -1,3 +1,4 @@
+// Задача 1. Усовершенствуйте кэширующий декоратор
 const addThree = (a, b, c) => (a + b + c); 
 const upgradedAddThree = cachingDecoratorNew(addThree);
 upgradedAddThree(1, 2, 3); 
@@ -9,12 +10,25 @@ upgradedAddThree(5, 2, 3);
 upgradedAddThree(6, 2, 3); 
 upgradedAddThree(1, 2, 3); 
 
-const sendSignal = () => console.log('Сигнал послан'); 
+// Задача 2. Фильтрация и преобразование массива
+const sendSignal = (number) => console.log(`Сигнал ${number} отправлен`); 
 const upgradedSendSignal = debounceDecoratorNew(sendSignal,2000);
-setTimeout(upgradedSendSignal()); 
-setTimeout(upgradedSendSignal(),300); 
-setTimeout(upgradedSendSignal(),900); 
-setTimeout(upgradedSendSignal(),1200);
-setTimeout(upgradedSendSignal(),2300);
-setTimeout(upgradedSendSignal(),4400);
-setTimeout(upgradedSendSignal(),4500);
+setTimeout(() => upgradedSendSignal(1)); // Сигнал отправлен
+setTimeout(() => upgradedSendSignal(2),300); // проигнорировано
+setTimeout(() => upgradedSendSignal(3),900); // проигнорировано
+setTimeout(() => upgradedSendSignal(4),1200); // проигнорировано
+setTimeout(() => upgradedSendSignal(5),2300); // Сигнал отправлен
+setTimeout(() => upgradedSendSignal(6),4400); // Сигнал отправлен
+setTimeout(() => upgradedSendSignal(7),4500); // проигнорировано
+
+// Задача 3. Усовершенствуйте debounceDecoratorNew
+const upgradedSendSignal2 = debounceDecorator2(sendSignal,2000);
+setTimeout(() => upgradedSendSignal2(1)); // Сигнал отправлен
+setTimeout(() => upgradedSendSignal2(2),300); // проигнорировано
+setTimeout(() => upgradedSendSignal2(3),900); // проигнорировано
+setTimeout(() => upgradedSendSignal2(4),1200); // проигнорировано
+setTimeout(() => upgradedSendSignal2(5),2300); // Сигнал отправлен
+setTimeout(() => upgradedSendSignal2(6),4400); // Сигнал отправлен
+setTimeout(() => upgradedSendSignal2(7),4500); // проигнорировано
+
+setTimeout(() => console.log(`Насчитано ${upgradedSendSignal2.count} вызовов upgradedSendSignal2`),5000);
--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,7 +1,57 @@
+// Задача 1. Усовершенствуйте кэширующий декоратор
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+    const hash = args.join(",");
+    const found = cache.find(item => item[hash] !== undefined);
+    if(found !== undefined) {
+      console.log("Из кеша: ", found[hash]);
+    } else {
+      let result = func(...args);
+      if(cache.length === 5) {
+        cache.shift(1);
+      }
+      cache.push({[hash]: result});
+      console.log("Вычисляем: " + result);
+    }
+  }
+  return wrapper;
 }
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+// Задача 2. Фильтрация и преобразование массива
+function debounceDecoratorNew(originalFunction, delay) {
+  let isDelayed = false;
+
+  return function (...args) {
+    // console.log(`новый вызов. Аргументы: ${args}.  Задержка: ${delay}.  Флаг: ${isDelayed}`);
+    if(!isDelayed) {
+      originalFunction(...args);
+      isDelayed = true;
+      setTimeout(() => {
+        // console.log(`Вышло время моратория ${delay}. Значение флага: ${isDelayed}`);
+        isDelayed = false;
+      }, delay);
+    }
+
+  };
+}
+
+// Задача 3. Усовершенствуйте debounceDecoratorNew
+function debounceDecorator2(originalFunction, delay) {
+  let isDelayed = false;
+
+  function wrapper(...args) {
+    wrapper.count++;
+    // console.log(`новый вызов. Аргументы: ${args}.  Задержка: ${delay}.  Счётчик: ${wrapper.count}`);
+    if(!isDelayed) {
+      originalFunction(...args);
+      isDelayed = true;
+      setTimeout(() => {
+        isDelayed = false;
+      }, delay);
+    }
+  };
+
+  wrapper.count = 0;
+  return wrapper;
 }
