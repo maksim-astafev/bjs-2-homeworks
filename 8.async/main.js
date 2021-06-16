d6e1d59ee404b3924aaae5656050c14bb9961fb5
Maksim Astafev <maksim.astafev@gmail.com>
2021-06-16 19:47:06 +0300

    ДЗ-8 выполнено в task.js и проверка в main.js

--- 8.async/main.js
+++ 8.async/main.js
@@ -1,1 +1,32 @@
-// тут пусто, так как нет UI
+function testCase() {
+  const alarms = new AlarmClock();
+  const safeAddClock = alarms.safeAddClock.call(alarms, alarms.addClock);
+  
+  console.log(`Текущее время ${alarms.getCurrentFormattedTime()}`);
+  
+  const now = new Date();
+  const time1 = now.getTime() + 1000 * 60 * 1;
+  const time2 = now.getTime() + 1000 * 60 * 2;
+  const time3 = now.getTime() + 1000 * 60 * 3;
+
+  safeAddClock(alarms, alarms.getFormattedTime(time1), () => console.log("Пора вставать!"), 1);
+  safeAddClock(alarms, alarms.getFormattedTime(time3), () => console.log("Уже точно пора вставать!"), 2);
+  safeAddClock(alarms, alarms.getFormattedTime(time2), () => console.log("Срочно вставай, а то проспишь!"), 3);
+  safeAddClock(alarms, alarms.getFormattedTime(time1), () => console.log("Пора умываться"));
+  safeAddClock(alarms, alarms.getFormattedTime(time1), () => console.log("Для надёжности ещё один будильник 1"), 1);
+  alarms.printAlarms();
+
+  alarms.removeClock(4);
+  alarms.removeClock(2);
+  alarms.printAlarms();
+
+  alarms.start();
+
+  setTimeout(() => alarms.removeClock(1), 1000 * 60 * 1 + 5000);
+  setTimeout(() => alarms.stop(), 1000 * 60 * 2 + 2000);
+  setTimeout(() => alarms.printAlarms(), 1000 * 60 * 2 + 3000);
+  setTimeout(() => alarms.clearAlarms(), 1000 * 60 * 2 + 4000);
+  setTimeout(() => alarms.printAlarms(), 1000 * 60 * 2 + 5000);
+}
+
+testCase();
---
+++ 8.async/task.js
@@ -0,0 +1,101 @@
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.timerId = null;
+    this.time = "time";
+    this.callback = "callback";
+    this.id = "id";
+    this.loopId = null;
+    this.loopInterval = 1000;
+  }
+
+  addClock(alarmTime, alarmAction, alarmId) {
+    if(alarmId === undefined) {
+      throw new Error("Новый будильник необходимо добавлять с указанием его №");
+    } else {
+      if(this.alarmCollection.find(alarm => alarm[this.id] === alarmId)) {
+        console.error(`Будильник №${alarmId} уже существует, добавление нового с таким же № не возможно`);
+        return false;
+      }
+    }
+    this.alarmCollection.push({[this.time]: alarmTime, [this.callback]: alarmAction, [this.id]: alarmId});
+    return true;
+  }
+
+  removeClock(alarmId) {
+    const oldLength = this.alarmCollection.length;
+    this.alarmCollection = this.alarmCollection.filter(item => item[this.id] !== alarmId);
+    if(this.alarmCollection.length === oldLength) {
+      console.warn(`Будильник №${alarmId} не найден`);
+      return false;
+    } else {
+      console.log(`Будильник №${alarmId} удалён`);
+      return true;
+    }
+  }
+
+  getCurrentFormattedTime() {
+    const now = new Date();
+    return this.getFormattedTime(now.getTime());
+  }
+
+  start() {
+    function checkClock(alarm) {
+      if(alarm[this.time] === this.getCurrentFormattedTime()) {
+        alarm[this.callback]();
+      }
+    }
+    checkClock = checkClock.bind(this);
+
+    if(this.loopId === null) {
+      this.loopId = setInterval(() => this.alarmCollection.forEach(alarm => checkClock(alarm)), this.loopInterval);
+    }
+  }
+
+  stop() {
+    if(this.loopId !== null) {
+      clearInterval(this.loopId);
+      this.loopId = null;
+
+      console.log("Все будильники остановлены");
+    }
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection = [];
+
+    console.log("Все будильники удалены");
+  }
+
+  printAlarms() {
+    const num = this.alarmCollection.length;
+    if(num > 0) {
+      console.log(`Список всех будильников в количестве ${num}: `);
+      this.alarmCollection.forEach(alarm => console.log(`Будильник №${alarm[this.id]} установлен на ${alarm[this.time]}`));
+    } else {
+      console.log(`Список установленных будильников пуст`);
+    }
+  }
+
+  safeAddClock(fn) {
+    return function (ptr, ...args) {
+      let result;
+      try {
+        result = fn.call(ptr, ...args);
+      } catch(err) {
+        console.error(err);
+      }
+      return result;
+    }
+  }
+ 
+  getFormattedTime(dateTime) {
+    const dateObj = new Date(dateTime);
+    let minutes = dateObj.getMinutes();
+    if(minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    return dateObj.getHours() + ":" + minutes;
+  }
+}
